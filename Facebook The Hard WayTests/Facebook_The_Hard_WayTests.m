bed7f712f71479dec61a2e6bb72bffcce5bf2d30
Eliot Arntz <arntzel@gmail.com>
2013-12-01 15:59:45 +0100

    Initial Commit

---
+++ Facebook The Hard WayTests/Facebook_The_Hard_WayTests.m
@@ -0,0 +1,34 @@
+//
+//  Facebook_The_Hard_WayTests.m
+//  Facebook The Hard WayTests
+//
+//  Created by Eliot Arntz on 12/1/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Facebook_The_Hard_WayTests : XCTestCase
+
+@end
+
+@implementation Facebook_The_Hard_WayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
